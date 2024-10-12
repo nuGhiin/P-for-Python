c0c8ca20a1025c624399b6836c560371b7a4df12
nuGhinTanjid <tanjidit2@gmail.com>
2024-10-13 03:57:33 +0600

    f

---
+++ basic.py
@@ -0,0 +1,34 @@
+#taking input:
+input("Give me money: ")
+money = input("Give me money: ")
+print("Here is your money: ", money)
+print(type(money))
+
+#type-casting
+money_int=int(money)
+
+
+print(56/3) # vagfol
+print(56%3) # reminder
+print(56//3) #purno vagfol
+
+#power 
+power = 2**3
+print(power)
+
+#if-else condition:
+
+a = 2
+if a>2:
+    print('true')
+elif a>1:
+    print('ok')
+else:
+    print('false')
+
+
+#while-loop:
+num =1
+while num<=10:
+    print(num)
+    num+=1
