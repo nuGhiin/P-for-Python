4ef9193fc192247d48303d4de8988cd10eac28f2
nuGhinTanjid <tanjidit2@gmail.com>
2024-10-23 14:49:14 +0600

    cv2

--- basic.py
+++ basic.py
@@ -290,3 +290,10 @@ for i in range(0, 3):
     pyautogui.press('enter')
 
 
+import cv2
+
+cam = cv2.VideoCapture(1)
+while True:
+    _, frame = cam.read()
+    cv2.imshow('my cam',frame)
+    cv2.waitKey(1)
