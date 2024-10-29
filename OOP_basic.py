b33499bd2ac08d5973a960391590d70a5712cc3c
nuGhinTanjid <tanjidit2@gmail.com>
2024-10-29 11:35:58 +0600

    attribute

--- OOP_basic.py
+++ OOP_basic.py
@@ -43,3 +43,41 @@ class Phone:
 
 tonmoyPhone = Phone('Tonmoy', 'Samsung', 16000)
 print(tonmoyPhone.owner, tonmoyPhone.brand, tonmoyPhone.price)
+
+
+""" class attributes v/s instance attributes """
+
+class Shop:
+    cart = [] # class attribute #shared by all instance of the shop
+
+    def __init__(self, buyer):
+        self.buyer = buyer
+    
+    def addToCart(self, item):
+        self.cart.append(item)
+
+tonmoy = Shop('ton moy')
+tonmoy.addToCart('watch')
+print(tonmoy.cart)
+
+tanjid = Shop('tan jid')
+tanjid.addToCart('car')
+print(tanjid.cart)
+
+class Shop:
+    mall = 'Jamuna'
+    
+    def __init__(self, buyer):
+        self.buyer =buyer
+        self.cart = [] #instance atribute
+    def addToCart(self,item):
+        self.cart.append(item)
+    
+
+tonjid = Shop('ton jid')
+tonjid.addToCart('house')  
+print(tonjid.cart)
+
+tanmoy = Shop('tan moy')
+tanmoy.addToCart('plot')
+print(tanmoy.cart)
