fd598d1be1347b6104d0ca5238fb24886e445c55
nuGhinTanjid <tanjidit2@gmail.com>
2024-11-05 13:32:48 +0600

    practice class

--- OOP_basic.py
+++ OOP_basic.py
@@ -81,3 +81,35 @@ print(tonjid.cart)
 tanmoy = Shop('tan moy')
 tanmoy.addToCart('plot')
 print(tanmoy.cart)
+
+###         Shopping Checkout & Price Calculation            ###
+
+class Shopping:
+    def __init__(self, name):
+        self.name = name
+        self.cart = []
+    
+    def addToCart(self, item, price, quantity):
+        product = {'item': item, 'price': price, 'quantity': quantity}
+        self.cart.append(product)
+
+    def checkout(self, amount):
+        total = 0
+        for item in self.cart:
+            total += item['price'] * item['quantity']
+        print('total price: ', total)
+        if amount <total:
+            print( f'please provide {total - amount} more')
+        else:
+            extra = amount -total
+            print(f'Here is your items and remaining {extra}')
+
+
+
+tonmoy = Shopping('T Tonmoy')
+tonmoy.addToCart('alu', 50, 6)
+tonmoy.addToCart('bread', 25, 5)
+
+print(tonmoy.cart)
+
+tonmoy.checkout(1469)
