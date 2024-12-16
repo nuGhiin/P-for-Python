6f37632e08f07dd60dbe50309f5239938bb6b9a3
nuGhinTanjid <tanjidit2@gmail.com>
2024-12-16 17:37:18 +0600

    modified

--- OOP Projects/School Management/person.py
+++ OOP Projects/School Management/person.py
@@ -19,7 +19,7 @@ class Student(Person):
         self.marks = {}
         self.subject_grade = {}
         self.grade = None
-    def final_grade(self) :
+    def calculate_final_grade(self) :
         sum = 0
         for grade in self.subject_grade.values():
             point = School.grade_to_value(grade) #5.00
@@ -30,7 +30,7 @@ class Student(Person):
     @property
     def id (self):
         return self.__id
-        
+
     @id.setter
     def id(self, value):
         self.__id = value
